e16084460994419cbbdfcb10a3235a79ea6c03ba
stanislavshabalinn <shabalin_sa1@mail.ru>
2025-02-28 15:00:47 +0700

    Добавьте файлы проекта.

---
+++ LifeSpot/Program.cs
@@ -0,0 +1,28 @@
+
+using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using static LifeSpot.Logger;
+
+namespace LifeSpot
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Выводим информационное сообщение
+            PrintMessage((() => Info("Запускаем приложение")));
+
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        private static void Target(string str)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+    }
+}
